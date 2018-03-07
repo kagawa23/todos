9d5834844cae1f871a638d41a1a36e1049d6db03
zhaochen <zhaochen01@juran.com.cn>
2018-03-07 15:31:46 +0800

    add eslint

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,7 @@
-import React from 'react';
-
-
 const App = () =>(
     <div>
         Hello world!
     </div>
 );
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App.js';
 
 ReactDOM.render(
-<Provider>
-<App />
-</Provider>, 
-document.getElementById('root'));
+    <Provider>
+    <App />
+    </Provider>, 
+    document.getElementById('root'));
